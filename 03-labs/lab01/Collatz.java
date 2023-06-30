7214572a76a8d39eec1b24250513ec3bdccd95e2
andrewnachtigal <ajnacht@gmail.com>
2023-06-30 15:43:47 -0700

    lab01_added

---
+++ 03-labs/lab01/Collatz.java
@@ -0,0 +1,26 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
+public class Collatz {
+
+    /** Returns the nextNumber in a Collatz sequence. */
+    public static int nextNumber(int n) {
+        /** description comment */
+        // TODO: Fill in this method.
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+
+        // Some starter code to test
+        while (n != 1) {          
+            n = nextNumber(n);          
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+    }
+}
+
